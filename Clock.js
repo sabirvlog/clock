374a23faec7d5586d11695e4d0e742cb4dfa7d9b
sabirvlog <sabirkhan0342464@gmail.com>
2022-05-10 10:23:33 +0500

    clock

---
+++ Clock.js
@@ -0,0 +1,20 @@
+setInterval(setClock,1000) /*we set the interval for a certain function because we want to call a certain function every second and we are just going to call this set o'clock for every 1000 miliseconds we are going to call this function. */
+
+ const hourHand = document.querySelector('[date-hour-hand]')
+ const minuteHand = document.querySelector('[date-minute-hand]')
+ const secondHand = document.querySelector('[date-second-hand]')
+
+function setClock(){
+	const currentDate = new Date()
+	const secondsRatio= currentDate.getSeconds() /60
+	const minutesRatio= (secondsRatio + currentDate.getMinutes()) /60
+	const hoursRatio= (minutesRatio + currentDate.getHours()) /12
+	setRotation(secondHand, secondsRotation)
+	setRotation(minuteHand, minutesRotation)
+	setRotation(hourHand, hoursRotation)
+}
+
+function setRotation(element, rotationRatio) {
+	element.style.setProperty('--rotation', rotationRatio * 360)
+}
+setClock()
